a5a3a06a60e7c76c912a7484e395f4d6f988cdf9
root <root@test.com>
2017-06-26 22:11:53 +0800

    more realm and roles

--- WebContent/list.jsp
+++ WebContent/list.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+	
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -9,6 +10,14 @@
 <body>
 	<h4>List Page</h4>
 	<a href="shiro/logout">Logout</a>
-	
+	<br />
+	<br />
+	<a href="admin.jsp">Admin Page</a>
+	<br />
+	<br />
+	<a href="user.jsp">User Page</a>
+	<br />
+	<br />
+
 </body>
 </html>
--- src/com/ly/shiro/realms/ShiroRealm.java
+++ src/com/ly/shiro/realms/ShiroRealm.java
@@ -1,5 +1,8 @@
 package com.ly.shiro.realms;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
@@ -7,11 +10,14 @@ import org.apache.shiro.authc.LockedAccountException;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.crypto.hash.SimpleHash;
-import org.apache.shiro.realm.AuthenticatingRealm;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.util.ByteSource;
 
-public class ShiroRealm extends AuthenticatingRealm {
+public class ShiroRealm extends AuthorizingRealm {
 
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
@@ -59,12 +65,33 @@ public class ShiroRealm extends AuthenticatingRealm {
 	public static void main(String[] args) {
 		String hashAlgorithmName = "MD5";
 		Object credentials = "123456";
-		 Object salt = ByteSource.Util.bytes("user");
-		//Object salt = null;
+		Object salt = ByteSource.Util.bytes("user");
+		// Object salt = null;
 		int hashIterations = 1024;
 
 		Object result = new SimpleHash(hashAlgorithmName, credentials, salt, hashIterations);
 		System.out.println(result);
+	}
+
+	// 用于授权的方法.
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+		System.out.println("doGetAuthorizationInfo......");
+		// 1. 从 PrincipalCollection 中来获取登录用户的信息
+		Object principal = principals.getPrimaryPrincipal();
+
+		// 2. 利用登录的用户的信息来用户当前用户的角色或权限(可能需要查询数据库)
+		Set<String> roles = new HashSet<>();
+		roles.add("user");
+		if ("admin".equals(principal)) {
+			roles.add("admin");
+		}
+
+		// 3. 创建 SimpleAuthorizationInfo, 并设置其 reles 属性.
+		SimpleAuthorizationInfo info = new SimpleAuthorizationInfo(roles);
+
+		// 4. 返回 SimpleAuthorizationInfo 对象.
+		return info;
 	}
 
 }
